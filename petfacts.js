59344947a80a5119e303bc35af2b38692b49370a
devihall <devis0306@gmail.com>
2022-07-24 18:39:40 -0400

    innerhtml display random dog img

--- petfacts.js
+++ petfacts.js
@@ -1,5 +1,7 @@
 var getDogFactsButton = document.querySelector (".btn")
 var queryUrl = "https://dog.ceo/api/breeds/image/random";
+var dogContainerEl = document.querySelector(".dog-container")
+var parentContainerEl = document.querySelector(".parent-container");
 
 
 //////////fetching random dog pic///////////
@@ -9,9 +11,20 @@ fetch(queryUrl).then (function (response){
     if (response.ok){
         response.json().then(function(data){
             console.log(data)
+
+            displayRandomDogPic(data)
         })
     }
 })
 
 }
 getRandomDogPic();
+
+function displayRandomDogPic(data){
+    console.log(data.message)
+    dogContainerEl.innerHTML = 
+    "<p>" + "Something" + "</p>"
+
+    parentContainerEl.append(dogContainerEl);
+//   console.log(dogContainerEl)
+}
