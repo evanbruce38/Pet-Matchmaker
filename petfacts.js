bcee2b9b945e60fe04b7ee7f8b667e0d8a568ae5
devihall <devis0306@gmail.com>
2022-07-24 17:39:49 -0400

    fetch request working

--- petfacts.js
+++ petfacts.js
@@ -1,8 +1,17 @@
 var getDogFactsButton = document.querySelector (".btn")
 var queryUrl = "https://dog.ceo/api/breeds/image/random";
+
+
+//////////fetching random dog pic///////////
 function getRandomDogPic(){
 fetch(queryUrl).then (function (response){
     console.log(response)
+    if (response.ok){
+        response.json().then(function(data){
+            console.log(data)
+        })
+    }
 })
 
 }
+getRandomDogPic();
