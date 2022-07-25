80b010c9b993a14c3ad1c2956add382a3302775c
devihall <devis0306@gmail.com>
2022-07-25 15:03:10 -0400

    random dog imgae is working

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,42 +1,45 @@
 /////////global variables/////////
-var searchDogsButton = document.querySelector(".btn");
+var searchDogsButton = document.querySelector(".search-button");
 var dogQueryUrl = "https://dog.ceo/api/breeds/image/random";
 var dogContainerEl = document.createElement("img");
 var dogParentContainerEl = document.querySelector(".dog-parent-container");
 var dogFactsParentContainer = document.querySelector(".parent-dog-facts");
+var placeholderImg = document.querySelector(".placeholder-img");
 var dogFactsContainer = document.createElement("p");
 
 // var catApiKey = "806b4584-de1a-4111-93e7-df9074e55aba";
 // var catQueryUrl = "https://api.thecatapi.com/v1/images?api_key=" + catApiKey;
-var catContainerEl = document.createElement("img");
-var catParentContainerEl = document.querySelector(".cat-parent-container");
+// var catContainerEl = document.createElement("img");
+// var catParentContainerEl = document.querySelector(".cat-parent-container");
 
-//////Event Listener for the get pet facts button//////
-searchDogsButton.addEventListener("click", function () {
+////Event Listener for the get pet facts button//////
+searchDogsButton.addEventListener("click", function (event) {
+    event.preventDefault()
     console.log("click")
   getRandomDogPic();
-  getRandomDogFacts();
+//   getRandomDogFacts();
 });
 
-//////////fetching random dog pic///////////
+////////fetching random dog pic///////////
 function getRandomDogPic() {
   fetch(dogQueryUrl).then(function (response) {
       console.log(response);
     if (response.ok) {
       response.json().then(function (data) {
-        //   console.log(data);
+          console.log(data);
 
         displayRandomDogPic(data);
       });
     }
   });
 }
-/////////////display random dog pic//////////
+// /////////////display random dog pic//////////
 function displayRandomDogPic(data) {
   // console.log(data.message)
+//   dogParentContainerEl.replace();
   dogParentContainerEl.append(dogContainerEl);
   dogContainerEl.setAttribute("src", data.message);
   dogContainerEl.className = "card stacked card_img";
 }
 
-///
+
