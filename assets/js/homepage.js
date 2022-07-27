3ff21103fee084f2f412238321919bc90d6874c3
devihall <devis0306@gmail.com>
2022-07-27 10:57:05 -0400

    saving searches to local history

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -45,7 +45,7 @@ function displayRandomDogPic(data) {
   dogContainerEl.className = "card stacked card_img";
  
 }
-
+////////////////////////////
 var mySearchHist = []
 var previousSearchHistory = JSON.parse(localStorage.getItem("history"))
 if (previousSearchHistory != null){
@@ -63,55 +63,10 @@ var searchHistoryList = function(breedName) {
 }
 localStorage.setItem('history',JSON.stringify(mySearchHist));
 
-////////////////////////////////////////
-
-  // create entry with breed name
-  var searchHistoryEntry = $("<p>");
-  searchHistoryEntry.addClass("past-search");
-  searchHistoryEntry.text(breedName);
-
-  // create container for entry
-  var searchEntryContainer = $("<div>");
-  searchEntryContainer.addClass("past-search-container");
-
-  // append entry to container
-  searchEntryContainer.append(searchHistoryEntry);
-
-  // append entry container to search history container
-  var searchHistoryContainerEl = $("#search-history-container");
-  searchHistoryContainerEl.append(searchEntryContainer);
-
-  if (savedSearches.length > 0){
-      // update savedSearches array with previously saved searches
-      var previousSavedSearches = localStorage.getItem("savedSearches");
-      savedSearches = JSON.parse(previousSavedSearches);
   }
 
-  // add breed name to array of saved searches
-  savedSearches.push(breedName);
-  localStorage.setItem("savedSearches", JSON.stringify(savedSearches));
+  
 
-  // reset search input
-  $("#search-input").val("");
 
-};
-
-// load saved search history entries into search history container
-var loadSearchHistory = function() {
-  // get saved search history
-  var savedSearchHistory = localStorage.getItem("savedSearches");
-
-  // return false if there is no previous saved searches
-  if (!savedSearchHistory) {
-      return false;
-  }
-
-  // turn saved search history string into array
-  savedSearchHistory = JSON.parse(savedSearchHistory);
-
-  // go through savedSearchHistory array and make entry for each item in the list
-  for (var i = 0; i < savedSearchHistory.length; i++) {
-      searchHistoryList(savedSearchHistory[i]);
-  }
-};
+  
 
