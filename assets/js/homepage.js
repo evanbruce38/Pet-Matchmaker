ba7ba520fb57bb5412cc5f5a453c5f646665ba70
devihall <devis0306@gmail.com>
2022-07-28 14:43:31 -0400

    recent searches button working

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -12,15 +12,7 @@ var searchContainer = document.querySelector(".previous-search-container");
 
 
 
-////Event Listener for the get pet facts button//////
-searchDogsButton.addEventListener("click", function (event) {
-    event.preventDefault()
-    console.log("click")
-    var breedInput = document.querySelector(".breed-input").value;
-  getRandomDogPic(breedInput);
-//   getRandomDogFacts();
-    searchHistoryList(breedInput);
-});
+
 
 ////////fetching random dog pic///////////
 function getRandomDogPic(breedInput) {
@@ -49,13 +41,24 @@ function displayRandomDogPic(data) {
  
 }
 /////////////////
-var mySearchHist = []
-var previousSearchHistory = JSON.parse(localStorage.getItem("history"))
+var mySearchHist = [];
+var previousSearchHistory = JSON.parse(localStorage.getItem("history"));
+
 if (previousSearchHistory != null){
-    mySearchHist= previousSearchHistory
-    
-    searchContainer.append(previousSearchHistory)
-   document.createElement("button");
+    mySearchHist = [...previousSearchHistory];
+    mySearchHist.forEach(function(savedPet, i) {
+      console.log(savedPet);
+      let petButton = document.createElement("button");
+      petButton.textContent = savedPet;
+      petButton.setAttribute("id", `petButton${i}`);
+      searchContainer.append(petButton);
+      petButton.addEventListener("click", function(event){
+        // comment out the prevent default to see if it is needed, later...
+        event.preventDefault();
+        getRandomDogPic(petButton.textContent);
+      })
+    })
+
 }
 
 /////////// Search history saving //////////
@@ -72,7 +75,15 @@ localStorage.setItem('history',JSON.stringify(mySearchHist));
   }
 
   
-
+////Event Listener for the get pet facts button//////
+searchDogsButton.addEventListener("click", function (event) {
+    event.preventDefault()
+    console.log("click")
+    var breedInput = document.querySelector(".breed-input").value;
+  getRandomDogPic(breedInput);
+//   getRandomDogFacts();
+    searchHistoryList(breedInput);
+});
 
   
 
